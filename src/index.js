3cbb56149dfe4a9f9fc0da7c7538893632773f0b
cosmicparticle <wnq111@126.com>
2023-05-30 15:26:41 +0800

    完善

--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ import "./style/common.css"
 import "./style/loading.css"
 import {loadableReady} from '@loadable/component';
 import ProgramConfigJson from "../public/programConfig.json";
-import {ProgramConfig} from 'aldehyde';
-import Welcome from './components/welcome';
+import {HydrocarbonService} from "aldehyde";
 
-loadableReady(() => {
+loadableReady(async () => {
     const root = createRoot(document.getElementById('root'));
-    ProgramConfig.setHydrocarbonServer(ProgramConfigJson.hydrocarbonServer);
-    ProgramConfig.setProgramCode(ProgramConfigJson.programCode)
-    ProgramConfig.setProgramName(ProgramConfigJson.programName)
-    ProgramConfig.setProgramName_NavLeft(ProgramConfigJson.programName_NavLeft)
-    ProgramConfig.setCustomPage(ProgramConfigJson.programCode, Welcome);
+    await HydrocarbonService.initProgramConfig(ProgramConfigJson);
     root.render(<HydrocarbonRouter/>)
 })
 
